e68e8b7b2211ce2861df084dcdfc2e82255e7442
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2021-11-19 21:21:34 -0300

    capturando o histórico

--- webmon.js
+++ webmon.js
@@ -1,3 +1,17 @@
-setInterval(() => {
-  console.log('TESTE');
-}, 1000);
+function printUrls(historyItems) {
+  for (var i = 0; i < historyItems.length; ++i) {
+    let url = historyItems[i].url;
+    
+    let time = new Date(historyItems[i].lastVisitTime);
+    
+    let data = time.toLocaleDateString('pt-BR');
+    
+    let hour = time.toLocaleTimeString('pt-BR');
+
+    let mysql = `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}`;
+
+    alert(`${url} \n---\n ${mysql} ${hour}`);
+  }
+}
+
+chrome.history.search({ text: '', startTime: 0, maxResults: 999999 }, printUrls);
